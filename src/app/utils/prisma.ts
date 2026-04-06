c33aa389db00d9eb75068defb3812cf9a4ea9423
CJ-Uy <75460325+CJ-Uy@users.noreply.github.com>
2026-04-06 12:26:36 +0800

    feat: replace prisma singleton with getPrisma() d1 factory

--- src/app/utils/prisma.ts
+++ src/app/utils/prisma.ts
@@ -1,3 +1,9 @@
+import { getCloudflareContext } from "@opennextjs/cloudflare";
 import { PrismaClient } from "@prisma/client";
+import { PrismaD1 } from "@prisma/adapter-d1";
 
-export const prisma = new PrismaClient();
+export async function getPrisma() {
+  const { env } = await getCloudflareContext({ async: true });
+  const adapter = new PrismaD1(env.DB);
+  return new PrismaClient({ adapter });
+}
